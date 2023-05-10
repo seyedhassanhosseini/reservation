f9aecb981e4869ad47d3a9d5eb3de8b2ff1d2e43
seyedhassanhosseini <seyedhassan.hoseiny@gmail.com>
2023-05-10 09:46:52 +0200

    new

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,9 +11,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
 
 
 const Header = () => {
+
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    }
+  ])
+
   return (
     <div className="header">
         <div className="headerContainer">
@@ -48,16 +62,27 @@ const Header = () => {
             <input type="text" placeholder="where are you going ?" className="headerSearchInput"/>
           </div>
           <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
-            <span className="headerSearchText">date to date</span>
+            <FontAwesomeIcon onClick={() => setOpenDate(!openDate)} icon={faCalendarDays} className="headerIcon"/>
+            <span onClick={() => setOpenDate(!openDate)} className="headerSearchText">{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")} `}</span>
+            {openDate && 
+                <DateRange
+                        editableDateInputs={true}
+                        onChange={(item) => setDate([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={date}
+                        className="date"
+                        minDate={new Date()}
+                      />        
+            }
           </div>
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faPerson} className="headerIcon" />
             <span className="headerSearchText"> adults 2 children 1 room</span>
           </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
+          </div>
         </div>
-
-
         </div>      
     </div>
   )
